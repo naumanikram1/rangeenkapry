d4730bf6157c556372e439a45a8631963b3bed3c
naumanikram1 <naumanikram92@gmail.com>
2025-03-05 06:40:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Get buttons by their IDs
+const pakistanBtn = document.getElementById('pakistanBtn');
+const internationalBtn = document.getElementById('internationalBtn');
+
+// Add links to the buttons
+pakistanBtn.addEventListener('click', () => {
+  window.location.href = 'https://www.daraz.pk/shop/dds0xbms'; // Replace with your link
+});
+
+internationalBtn.addEventListener('click', () => {
+  window.location.href = 'https://www.ebay.com/usr/rangeenkapry'; // Replace with your link
+});
